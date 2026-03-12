2f0fa522fc18d238548f38f123aa88738f426634
LimnedMoonlight <limnedmoonlight@gmail.com>
2026-03-13 00:04:59 +0100

    Add files via upload

---
+++ NoVehicleNameDisplay.lua
@@ -0,0 +1,20 @@
+-- ============================================================
+-- FS25_NoVehicleNameDisplay  v1.0.0.0
+-- Autor: LimnedMoon
+--
+-- Entfernt die Fahrzeugname-Einblendung beim Einsteigen.
+-- Removes the vehicle name popup on vehicle enter.
+-- ============================================================
+
+local origLoadFinished = Mission00.loadMission00Finished
+Mission00.loadMission00Finished = function(mission, ...)
+    origLoadFinished(mission, ...)
+
+    if g_currentMission ~= nil
+            and g_currentMission.hud ~= nil
+            and g_currentMission.hud.vehicleNameDisplay ~= nil then
+        local vnd = g_currentMission.hud.vehicleNameDisplay
+        vnd.setVisible = function() end
+        vnd.update     = function() end
+    end
+end
